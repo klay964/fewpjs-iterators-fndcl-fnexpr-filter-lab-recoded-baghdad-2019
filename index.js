7584de63a949bd0f91629750ec1d77b4f29ddb6b
Mohammed Ali <dody200039@gmail.com>
2019-12-29 23:12:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution here
+let arr = [1, 2, 3, 4, 5, 6];
+let even = [];
+for(var i = 0; i < arr.length; i++) {
+  if (arr[i] % 2 === 0) even.push(arr[i]);
+  even.filter(n => {
+  return n % 2 === 0;
+});
+}
