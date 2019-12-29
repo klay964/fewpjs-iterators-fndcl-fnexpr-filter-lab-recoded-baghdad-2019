3abb8c0196e961a5d03c4b8e77687fbc00ad04dd
Mohammed Ali <dody200039@gmail.com>
2019-12-29 23:15:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-// Code your solution here
-let arr = [1, 2, 3, 4, 5, 6];
-let even = [];
-for(var i = 0; i < arr.length; i++) {
-  if (arr[i] % 2 === 0) even.push(arr[i]);
-  even.filter(n => {
-  return n % 2 === 0;
-});
+function findMatching (list, name) {
+  return list.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch (list, partialName) {
+  let ln = partialName.length;
+  return list.filter(function (driverName) {
+    return driverName.slice(0, ln) === partialName;
+  });
+}
+
+function matchName (list, name) {
+  return list.filter(function (driver) {
+    return driver.name.toLowerCase() === name.toLowerCase();
+  });
 }
